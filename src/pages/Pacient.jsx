49b2d800c009ab10e2d6384e5495c2c626084d52
Maksim Konkov <soft.snake867@gmail.com>
2019-07-27 20:23:34 +0500

    styles

--- src/pages/Pacient.jsx
+++ src/pages/Pacient.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import './css/header.css';
-import './css/pacient.css'
+import './css/main.css'
 
 class Pacient extends React.Component {
     render() {
@@ -9,16 +9,84 @@ class Pacient extends React.Component {
                 <header>
                     <div className="wrap">
                         <div className="block-left">
-                            <div className="column">
-                                <div className="title">
-                                    <h2>Гиппократ</h2>
-                                </div>
-                                <div className="subtitle">Электронная система здравоохранения</div>
+                            <div className="title">
+                                <h2>Гиппократ</h2>
+                            </div>
+                            <div className="subtitle">
+                                <p>Электронная система здравоохранения</p>
                             </div>
                         </div>
                     </div>
                 </header>
-                <div className="wrap">
+                <main>
+                    <div className="wrap main-wrap">
+                        <div className="pacient-data">
+                            <div className="pacient-logo"></div>
+                            <div className="pacient-name">
+                                Марина Ивановна Цветаева
+                            </div>
+                        </div>
+                        <div className="center">
+                            <div className="visits">
+                                <h2>Ближайшие посещения</h2>
+                                <hr className="line" />
+                                <div className="info-blocks">
+                                    <div className="info-block">
+                                        <h6>Дата</h6>
+                                        <p>14.08.2019, 16:30</p>
+                                        <a className="info-link" href="#">Отменить посещение</a>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Врач</h6>
+                                        <p>Николай Иванович Пирогов</p>
+                                        <a className="info-link" href="#">Перенести на другой день</a>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Специальность</h6>
+                                        <p>Хирург</p>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>ЛПУ</h6>
+                                        <p>МАУ ГКБ 14</p>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Статус</h6>
+                                        <p>Открыто</p>
+                                    </div>
+                                </div>
+                                <hr className="line"/>
+                                <div className="info-blocks">
+                                    <div className="info-block">
+                                        <h6>Дата</h6>
+                                        <p>14.08.2019, 16:30</p>
+                                        <a className="info-link" href="#">Отменить посещение</a>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Врач</h6>
+                                        <p>Николай Иванович Пирогов</p>
+                                        <a className="info-link" href="#">Перенести на другой день</a>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Специальность</h6>
+                                        <p>Хирург</p>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>ЛПУ</h6>
+                                        <p>МАУ ГКБ 14</p>
+                                    </div>
+                                    <div className="info-block">
+                                        <h6>Статус</h6>
+                                        <p>Открыто</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="quest"></div>
+                            <div className="feedback"></div>
+                        </div>
+                    </div>
+                </main>
+
+                {/* <div className="wrap">
                     <div class="pat">
                         <p class="Name">Марина Ивановна Цветаева </p>
                         <p class="date">Дата рождения</p>
@@ -31,7 +99,7 @@ class Pacient extends React.Component {
                     <div class='bl'>Больничный лист</div>
                     <div class="ots"> Оставить отзыв</div>
                     <div class='cart'>Карта пациента</div>
-                </div>
+                </div> */}
             </Fragment>
         )
     }
