4d5e9cb329d618645122b893be19850ca020cc9a
Maksim Konkov <soft.snake867@gmail.com>
2019-07-27 19:26:58 +0500

    simplify

--- src/containers/Routes.jsx
+++ src/containers/Routes.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Auth from '../pages/Auth';
-import Admin from '../pages/Admin';
-import Doctor from '../pages/Doctor';
 import Pacient from '../pages/Pacient';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={Auth}/>
-      <Route exact path='/admin' component={Admin}/>
-      <Route exact path='/doctor' component={Doctor}/>
-      <Route exact path='/pacient' component={Pacient}/>
+      <Route exact path='/' component={Pacient}/>
     </Switch>
   </BrowserRouter>
 )
--- src/pages/Pacient.jsx
+++ src/pages/Pacient.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import './css/header.css';
+import './css/pacient.css'
 
 class Pacient extends React.Component {
     render() {
@@ -9,24 +10,28 @@ class Pacient extends React.Component {
                     <div className="wrap">
                         <div className="block-left">
                             <div className="column">
-                                <div className="logo">Лого</div>
-                            </div>
-                            <div className="column">
-                                <div className="title">Гиппократ</div>
+                                <div className="title">
+                                    <h2>Гиппократ</h2>
+                                </div>
                                 <div className="subtitle">Электронная система здравоохранения</div>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="column">
-                                <div className="name">name</div>
-                                <div className="logout">logout</div>
-                            </div>
-                            <div className="column">
-                                <div className="avatar">avatar</div>
                             </div>
                         </div>
                     </div>
                 </header>
+                <div className="wrap">
+                    <div class="pat">
+                        <p class="Name">Марина Ивановна Цветаева </p>
+                        <p class="date">Дата рождения</p>
+                        <p class="nom">Номер полиса</p>
+                        <p class="pol">Поликлиника</p>
+                    </div>
+                    <div class="ln">Ближайшие посещения</div>
+                    <div class="vn">Внимание</div>
+                    <div> </div>
+                    <div class='bl'>Больничный лист</div>
+                    <div class="ots"> Оставить отзыв</div>
+                    <div class='cart'>Карта пациента</div>
+                </div>
             </Fragment>
         )
     }
