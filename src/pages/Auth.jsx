dcb1c5d096e7d8e9ce0519cfda9a78e3b073d504
Maksim Konkov <soft.snake867@gmail.com>
2019-07-27 16:15:29 +0500

    routing

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,14 +1,63 @@
 import React, { Fragment } from 'react';
 import './css/auth.css'
+import loginData from '../temp_data/login';
+import { Redirect } from 'react-router';
 
 class Auth extends React.Component {
+    state = {
+        loginValue: '',
+        passwordValue: '',
+        route: '/',
+        userType: '',
+        redirect: false,
+    }
+
+    updatePasswordValue = (e) => {
+        this.setState({
+            passwordValue: e.target.value,
+        })
+    }
+
+    updateLoginValue = (e) => {
+        this.setState({
+            loginValue: e.target.value,
+        })
+    }
+
+    checkLogin = (e) => {
+        e.preventDefault();
+        let password = this.state.passwordValue;
+        let login = this.state.loginValue;
+        let user = loginData.find(
+            function (user) {
+                return (user.login === login && user.password === password)
+            }
+        )
+        if (user) {
+            this.setState({
+                route: user.root,
+                redirect: true,
+            })
+        }
+    }
+
+    renderNext = () => {
+        if (this.state.redirect) {
+            return <Redirect to={this.state.route} />
+        }
+    }
+
+    loginInput = React.createRef();
+    passwordInput = React.createRef();
+
     render() {
         return (
             <Fragment>
                 <form className="login">
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <button>Login</button>
+                    <input autoComplete="off" type="text" placeholder="Username" ref={this.loginInput} onChange={this.updateLoginValue} />
+                    <input autoComplete="off" type="password" placeholder="Password" ref={this.passwordInput} onChange={this.updatePasswordValue} />
+                    <button onClick={this.checkLogin}>Login</button>
+                    {this.renderNext()}
                 </form>
             </Fragment>
         )
---
+++ src/temp_data/login.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        login: 'admin',
+        password: 'example1',
+        root: '/admin',
+        type: 'admin',
+    },
+    {
+        login: 'doctor',
+        password: 'example2',
+        root: '/doctor',
+        type: 'doctor',
+    },
+    {
+        login: 'pacient',
+        password: 'example3',
+        root: '/pacient',
+        type: 'pacient',
+    }
+]
