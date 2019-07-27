140f2b66a06f9ea285227c9f97e919bfbd0b069d
Maksim Konkov <soft.snake867@gmail.com>
2019-07-28 00:51:48 +0500

    radiobuttons

--- src/containers/Question/index.jsx
+++ src/containers/Question/index.jsx
@@ -2,11 +2,38 @@ import React, { Fragment, Component } from 'react';
 import './style.css'
 
 class Question extends Component {
+    state ={
+        yes: false,
+        no: false,
+    }
+
+    setYes = () => {
+        this.setState({
+            yes: true,
+            no: false,
+        })
+    }
+
+    setNo = () => {
+        this.setState({
+            yes: false,
+            no: true,
+        })
+    }
+
     render() {
         return (
             <Fragment>
                 <div className="question-body">
                     <p className="question">{this.props.question}</p>
+                    <label className="question-radio">
+                        <input className="question-var" type="radio" checked={this.state.yes} onClick={this.setYes} />
+                        Да
+                    </label>
+                    <label className="question-radio">
+                        <input className="question-var" type="radio" checked={this.state.no} onClick={this.setNo} />
+                        Нет
+                    </label>
                 </div>
             </Fragment>
         )
