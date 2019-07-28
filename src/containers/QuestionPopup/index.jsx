155b7eabf15a4a7db2bf27303cdd12e7595a1b04
Maksim Konkov <soft.snake867@gmail.com>
2019-07-28 12:18:31 +0500

    close popup

--- src/containers/QuestionPopup/index.jsx
+++ src/containers/QuestionPopup/index.jsx
@@ -24,31 +24,46 @@ const QUESTIONS = [
     },
 ]
 
-class Popup extends Component {
-    render() {
-        return (
-            <Fragment>
-                <div className="popup-body">
-                    <div className="all-form">
-                        <form className="popup-form">
-                            <h2 className="popup-title">Ответить на вопросы</h2>
-                            <div className="question-list">
-                                {QUESTIONS.map((task, index) => (
-                                    <Question key={index} question={task.question} number={index} />
-                                ))}
-                            </div>
-                            <div className="form-footer">
-                                <img src="images/button-blue.svg" alt="blue button" />
-                                <div className="addition-text-form">
-                                    <p>Есть что добавить?</p>
-                                    <a href="/">Дополнить ответ комментарием</a>
-                                </div>
-                            </div>
-                        </form>
-                        <img src="images/cross.png" alt="cross" className="cross" />
+const PopupBody = (props) => (
+    <div className="popup-body">
+        <div className="all-form">
+            <form className="popup-form">
+                <h2 className="popup-title">Ответить на вопросы</h2>
+                <div className="question-list">
+                    {QUESTIONS.map((task, index) => (
+                        <Question key={index} question={task.question} number={index} />
+                    ))}
+                </div>
+                <div className="form-footer">
+                    <img src="images/button-blue.svg" alt="blue button" />
+                    <div className="addition-text-form">
+                        <p>Есть что добавить?</p>
+                        <a href="/">Дополнить ответ комментарием</a>
                     </div>
                 </div>
+            </form>
+            <img src="images/cross.png" alt="cross" className="cross" onClick={props.handleClick} />
+        </div>
+    </div>
+)
 
+class Popup extends Component {
+    state = {
+        show: true,
+    }
+
+    handleClick = () => {
+        this.setState({
+            show: false,
+        })
+    }
+
+    render() {
+        const { show } = this.state;
+
+        return (
+            <Fragment>
+                {show ? <PopupBody handleClick={this.handleClick} /> : <div className="popup-none"></div>}
             </Fragment>
         )
     }
